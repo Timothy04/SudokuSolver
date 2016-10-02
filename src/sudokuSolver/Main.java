6bcefac9a068f9dea96ccbc42ca976e179a2740a
Timothy Van Peteghem <timothy_van_peteghem@hotmail.com>
2016-10-03 00:39:05 +0200

    Another try to commiy

--- src/sudokuSolver/Main.java
+++ src/sudokuSolver/Main.java
@@ -3,7 +3,7 @@ package sudokuSolver;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Sudoku Solver !!!");
+		System.out.println("Welcome to Sudoku Solver !!");
 		
 		Grid.printEmptyGrid();
 	}
